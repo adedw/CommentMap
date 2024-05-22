d7e7541be403950d7f5e5be83ff0d63800143c2c
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-05-22 21:20:29 +0500

    CM-21 Add serilog (#23)

--- CommentMap.Mvc/Program.cs
+++ CommentMap.Mvc/Program.cs
@@ -2,57 +2,76 @@ using CommentMap.Mvc.Data;
 using CommentMap.Mvc.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
 
-builder.Services
-    .AddDefaultIdentity<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.Configure<IdentityOptions>(options => options.Lockout.AllowedForNewUsers = false);
-builder.Services
-    .AddAuthentication()
-    .AddGoogle(googleOptions =>
+    // Add services to the container.
+    builder.Services.AddSerilog();
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services
+        .AddDefaultIdentity<IdentityUser>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.Configure<IdentityOptions>(options => options.Lockout.AllowedForNewUsers = false);
+    builder.Services
+        .AddAuthentication()
+        .AddGoogle(googleOptions =>
+        {
+            var googleAuthenticationOptions = builder.Configuration
+                .GetSection("Authentication:Google")
+                .Get<GoogleAuthenticationOptions>();
+            googleOptions.ClientId = googleAuthenticationOptions.ClientId;
+            googleOptions.ClientSecret = googleAuthenticationOptions.ClientSecret;
+        });
+
+    var mvcBuilder = builder.Services.AddRazorPages();
+
+    if (builder.Environment.IsDevelopment())
     {
-        var googleAuthenticationOptions = builder.Configuration
-            .GetSection("Authentication:Google")
-            .Get<GoogleAuthenticationOptions>();
-        googleOptions.ClientId = googleAuthenticationOptions.ClientId;
-        googleOptions.ClientSecret = googleAuthenticationOptions.ClientSecret;
-    });
+        mvcBuilder.AddRazorRuntimeCompilation();
+    }
 
-var mvcBuilder = builder.Services.AddRazorPages();
+    var app = builder.Build();
 
-if (builder.Environment.IsDevelopment())
-{
-    mvcBuilder.AddRazorRuntimeCompilation();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
+
 }
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.UseMigrationsEndPoint();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-else
+finally
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.CloseAndFlush();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
