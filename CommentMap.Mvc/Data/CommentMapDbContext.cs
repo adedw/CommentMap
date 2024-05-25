7ffa45bcde6e715f7e7474988be99efe2e3215bc
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-05-25 11:03:13 +0500

    remove explicit extension settings

--- CommentMap.Mvc/Data/CommentMapDbContext.cs
+++ CommentMap.Mvc/Data/CommentMapDbContext.cs
@@ -12,7 +12,6 @@ public class CommentMapDbContext(DbContextOptions<CommentMapDbContext> options) 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        builder.HasPostgresExtension("postgis");
         builder.ApplyConfiguration(new CommentConfiguration());
     }
 }
