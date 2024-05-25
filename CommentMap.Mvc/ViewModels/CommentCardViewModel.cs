aa626661cfd901c64553aa99000b167628c6cf41
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-05-26 00:24:37 +0500

    add comment card partial view

---
+++ CommentMap.Mvc/ViewModels/CommentCardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CommentMap.Mvc.ViewModels;
+
+public record CommentCardViewModel(
+    Guid Id,
+    Coordinates Location,
+    string Title,
+    string Text,
+    DateTime CreatedAt);
+
+public record Coordinates(decimal Latitude, decimal Longitude);
