590b5443e3b8f17a9a9f96bfb7043cb3d4a9702e
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-05-26 00:15:05 +0500

    add comment props

--- CommentMap.Mvc/Data/Entities/Configurations/CommentConfiguration.cs
+++ CommentMap.Mvc/Data/Entities/Configurations/CommentConfiguration.cs
@@ -15,7 +15,10 @@ public class CommentConfiguration : IEntityTypeConfiguration<Comment>
             .OnDelete(DeleteBehavior.Cascade);
 
         builder.Property(c => c.Location).HasColumnType("geometry (point)");
+        builder.HasIndex(c => c.Location).HasMethod("gist");
 
+        builder.Property(c => c.Title).HasMaxLength(250);
         builder.Property(c => c.Text).HasMaxLength(250);
+        builder.Property(c => c.CreatedAt).HasDefaultValueSql("NOW()");
     }
 }
