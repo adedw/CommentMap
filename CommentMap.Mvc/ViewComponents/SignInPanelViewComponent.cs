7571f9519cccbc37bb816f71982fd7749e504ff9
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-05-25 17:03:35 +0500

    add sign in panel view component

---
+++ CommentMap.Mvc/ViewComponents/SignInPanelViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using CommentMap.Mvc.Data.Entities;
+using CommentMap.Mvc.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentMap.Mvc.ViewComponents;
+
+public class SignInPanelViewComponent(SignInManager<User> signInManager) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var isSignedIn = signInManager.IsSignedIn(UserClaimsPrincipal);
+        return View(new SignInPanelViewModel(isSignedIn));
+    }
+}
