6132f4aa2d2d3f1253796e5e4011e8dfc3463151
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-06-09 18:36:53 +0500

    add displaying points on map

--- CommentMap.Mvc/Scripts/comments.ts
+++ CommentMap.Mvc/Scripts/comments.ts
@@ -1,19 +1,32 @@
 import Map from "ol/Map";
-import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
-import View from 'ol/View';
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { XYZ, Vector as VectorSource } from "ol/source";
+import { FullScreen, defaults as defaultControls } from "ol/control";
+import { MapViewModel } from "./MapViewModel";
+import type { Location } from "./types";
+
+const locations: Location[] = $("[data-location]").map(function () {
+  return $(this).data("location");
+}).get();
+
+const mapViewModel = MapViewModel.fromLocations(locations);
+
+const defaultView = mapViewModel.getViewAtFirst();
+
+const features = mapViewModel.getPointFeatures();
 
 const map = new Map({
-    target: "map",
-    layers: [
-        new TileLayer({
-            source: new XYZ({
-                url: 'http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}'
-            }),
-        })
-    ],
-    view: new View({
-        center: [0, 0],
-        zoom: 2,
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        url: "https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}"
+      }),
     }),
+    new VectorLayer({
+      source: new VectorSource({ features })
+    })
+  ],
+  view: defaultView,
+  controls: defaultControls().extend([new FullScreen()]),
 });
