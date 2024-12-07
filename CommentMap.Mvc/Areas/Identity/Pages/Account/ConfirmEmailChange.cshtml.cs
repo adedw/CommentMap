53c344f83642490460cc56fcb558dc52cdd6954e
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-12-07 16:41:38 +0500

    change update email page: send email confirmation link

--- CommentMap.Mvc/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ CommentMap.Mvc/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -7,16 +7,8 @@ using Microsoft.AspNetCore.WebUtilities;
 
 namespace CommentMap.Mvc.Areas.Identity.Pages.Account;
 
-public class ConfirmEmailChangeModel : PageModel
+public class ConfirmEmailChangeModel(UserManager<User> userManager, SignInManager<User> signInManager) : PageModel
 {
-    private readonly UserManager<User> _userManager;
-    private readonly SignInManager<User> _signInManager;
-
-    public ConfirmEmailChangeModel(UserManager<User> userManager, SignInManager<User> signInManager)
-    {
-        _userManager = userManager;
-        _signInManager = signInManager;
-    }
 
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -32,14 +24,14 @@ public class ConfirmEmailChangeModel : PageModel
             return RedirectToPage("/Index");
         }
 
-        var user = await _userManager.FindByIdAsync(userId);
-        if (user == null)
+        var user = await userManager.FindByIdAsync(userId);
+        if (user is null)
         {
             return NotFound($"Unable to load user with ID '{userId}'.");
         }
 
         code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-        var result = await _userManager.ChangeEmailAsync(user, email, code);
+        var result = await userManager.ChangeEmailAsync(user, email, code);
         if (!result.Succeeded)
         {
             StatusMessage = "Error changing email.";
@@ -48,14 +40,14 @@ public class ConfirmEmailChangeModel : PageModel
 
         // In our UI email and user name are one and the same, so when we update the email
         // we need to update the user name.
-        var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
+        var setUserNameResult = await userManager.SetUserNameAsync(user, email);
         if (!setUserNameResult.Succeeded)
         {
             StatusMessage = "Error changing user name.";
             return Page();
         }
 
-        await _signInManager.RefreshSignInAsync(user);
+        await signInManager.RefreshSignInAsync(user);
         StatusMessage = "Thank you for confirming your email change.";
         return Page();
     }
--- CommentMap.Mvc/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ CommentMap.Mvc/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,29 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Text;
 using CommentMap.Mvc.Data.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace CommentMap.Mvc.Areas.Identity.Pages.Account.Manage;
 
-public class IndexModel : PageModel
+public class IndexModel(
+    UserManager<User> userManager,
+    IEmailSender emailSender)
+    : PageModel
 {
-    private readonly UserManager<User> _userManager;
-    private readonly SignInManager<User> _signInManager;
-
-    public IndexModel(
-        UserManager<User> userManager,
-        SignInManager<User> signInManager)
-    {
-        _userManager = userManager;
-        _signInManager = signInManager;
-    }
-
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    public string Username { get; set; }
+    public string? Email { get; set; }
 
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -50,58 +46,67 @@ public class IndexModel : PageModel
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         [Required]
-        [Display(Name = "New username")]
-        public string NewUserName { get; set; }
+        [Display(Name = "New email")]
+        public string? NewEmail { get; set; }
     }
 
-    private async Task LoadAsync(User user)
+    private void Load(User user)
     {
-        var userName = await _userManager.GetUserNameAsync(user);
+        Email = user.Email;
 
-        Username = userName;
-
-        Input = new InputModel();
+        Input = new InputModel
+        {
+            NewEmail = user.Email,
+        };
     }
 
     public async Task<IActionResult> OnGetAsync()
     {
-        var user = await _userManager.GetUserAsync(User);
-        if (user == null)
+        var user = await userManager.GetUserAsync(User);
+        if (user is null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        await LoadAsync(user);
+        Load(user);
         return Page();
     }
 
-    public async Task<IActionResult> OnPostAsync()
+    public async Task<ActionResult> OnPostAsync()
     {
-        var user = await _userManager.GetUserAsync(User);
-        if (user == null)
+        var user = await userManager.GetUserAsync(User);
+        if (user is null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
         if (!ModelState.IsValid)
         {
-            await LoadAsync(user);
+            Load(user);
             return Page();
         }
 
-        var oldUserName = await _userManager.GetUserNameAsync(user);
-        if (Input.NewUserName != oldUserName)
+        var email = await userManager.GetEmailAsync(user);
+        if (Input.NewEmail == email)
         {
-            var identityResult = await _userManager.SetUserNameAsync(user, Input.NewUserName);
-            if (!identityResult.Succeeded)
-            {
-                StatusMessage = "Error when trying to update username.";
-                return RedirectToPage();
-            }
+            StatusMessage = "Your email is unchanged.";
+            return RedirectToPage();
         }
 
-        await _signInManager.RefreshSignInAsync(user);
-        StatusMessage = "Your profile has been updated";
+        var userId = user.Id;
+        var code = await userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        var callbackUrl = Url.Page(
+            "/Account/ConfirmEmailChange",
+            pageHandler: null,
+            values: new { area = "Identity", userId, email = Input.NewEmail, code },
+            protocol: Request.Scheme);
+        await emailSender.SendEmailAsync(
+            Input.NewEmail,
+            "Confirm your email",
+            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+        StatusMessage = "Confirmation link to change email sent. Please check your email.";
         return RedirectToPage();
     }
 }
