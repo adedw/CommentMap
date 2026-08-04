7717af2c8b9428c9bdc14ede0fe83784fefa201b
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2026-08-04 23:20:16 +0500

    Replace jQuery validation with aspnet-client-validation
    Switch client-side validation from jquery-validation(+unobtrusive) to aspnet-client-validation.

--- CommentMap.Mvc/build/build.js
+++ CommentMap.Mvc/build/build.js
@@ -5,7 +5,7 @@ await esbuild.build({
     "js/Comments.min": "./Scripts/Comments.ts",
     "js/AddComment.min": "./Scripts/AddComment.ts",
     "css/ol.min": "./node_modules/ol/ol.css",
-    "css/validation-summary-errors.min": "./Styles/validation-summary-errors.css"
+    "css/validation-errors.min": "./Styles/validation-errors.css"
   },
   bundle: true,
   minify: true,
